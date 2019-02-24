9fef07943846471103377a13b0ee4bae7ae6f1bd
enderst3 <enderst3@gmail.com>
2019-02-23 22:33:50 -0800

    webscrape

--- ds_dojo_webscrape.py
+++ ds_dojo_webscrape.py
@@ -3,3 +3,8 @@ Intro to web scraping with beautiful soup
 Data Science Dojo
 https://www.youtube.com/watch?v=XQgXKtPSzUI
 """
+
+from urllib.request import urlopen as uREq
+from bs4 import BeautifulSoup as soup
+
+my_url = 'http:www.newegg.com/Video-Cards-Video-Devices/Category/ID-38?Tpk=graphics%card'
