34c3aa0fbbd2c27e28c189ddd09d9033c0ba3f10
enderst3 <enderst3@gmail.com>
2019-02-25 17:43:11 -0800

    web scraping

--- ds_dojo_webscrape.py
+++ ds_dojo_webscrape.py
@@ -4,7 +4,20 @@ Data Science Dojo
 https://www.youtube.com/watch?v=XQgXKtPSzUI
 """
 
-from urllib.request import urlopen as uREq
+from urllib.request import urlopen as uReq
 from bs4 import BeautifulSoup as soup
 
 my_url = 'http:www.newegg.com/Video-Cards-Video-Devices/Category/ID-38?Tpk=graphics%card'
+
+# opening up connection, grabibng the page
+uClient = uReq(my_url)
+page_html = uClient.read()
+uClient.close()
+
+# html parsing
+page_soup = soup(page_html, "html.parser")
+
+# grabs each product
+containers = page_soup.findAll('div', {"class":"item-container"})
+
+container.div.div.a.img[title]
