03c5264ec579cad9f4c4e425679ba2c464d83f5c
enderst3 <enderst3@gmail.com>
2019-03-16 23:00:03 -0700

    numpy array logic

--- numpy_lists.py
+++ numpy_lists.py
@@ -1,3 +1,22 @@
+"""
+Numpy key points:
+
+- Arrays are a special type of list that allows us to store values in an organized manner.
+
+- An array can be created by either defining it directly using np.array() or by importing a CSV using 
+np.genfromtxt('file.csv', delimiter=',').
+
+- An operation (such as addition) can be performed on every element in an array by simply performing 
+it on the array itself.
+
+- lements can be selected from arrays using their index and array locations, both of which start at 0.
+
+- Logical operations can be used to create new, more focused arrays out of larger arrays.
+
+"""
+
+
+
 # import numpy 
 import numpy as np
 
